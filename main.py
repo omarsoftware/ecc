f920eb3e7c3b27a59fd8b63d8e68969835bef4f1
omar <OmarGH>
2021-11-02 22:42:05 -0300

    first try with new structure using classes in a different way

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-from tkinter import *
-from ecmath import *
-from start import *
+# Multi-frame tkinter application v2.3
+import tkinter as tk
+import constants as cons
 
-root = Tk()
-app = Start(root)
-app.start()
 
-'''
+class SampleApp(tk.Tk):
 
-'''
-root.mainloop()
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(StartPage)
+
+    def switch_frame(self, frame_class):
+        """Destroys current frame and replaces it with a new one."""
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        master.geometry(cons.screen_size)
+        tk.Label(self, text="This is the start page").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Open page one",
+                  command=lambda: master.switch_frame(PageOne)).pack()
+        tk.Button(self, text="Open page two",
+                  command=lambda: master.switch_frame(PageTwo)).pack()
+
+
+class PageOne(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        master.geometry(cons.screen_size)
+        tk.Label(self, text="This is page one").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(StartPage)).pack()
+
+
+class PageTwo(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        master.geometry(cons.screen_size)
+        tk.Label(self, text="This is page two").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(StartPage)).pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
