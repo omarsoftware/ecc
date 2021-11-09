643eaef030534b64c243c8e9bfbce7365e547c4c
omar <OmarGH>
2021-11-08 21:55:36 -0300

    haciendo primer pasabe de habilitación y deshabilitación entre los pasos 1 y 2

--- ecdh.py
+++ ecdh.py
@@ -9,7 +9,6 @@ class Ecdh:
         self.frame = tk.Frame(self.master)
         self.elliptic_curve = None
         self.g = ec.Point()
-        self.ec_step = {"labels": [], "entries": []}
 
         title = tk.Label(self.frame, text='Elliptic-curve Diffie-Hellman')
         title.grid(row=0, column=0, sticky="W")
@@ -20,16 +19,16 @@ class Ecdh:
         # ///////////// Begin Elliptic Curve /////////////
         self.ec_title = tk.Label(self.frame)
         self.ec_gen_eq = tk.StringVar()
-        self.ec_gen_eq_label = tk.Label(self.frame, textvariable=self.ec_gen_eq)
+        self.ec_gen_eq_label = tk.Label(self.frame)
         self.ec_frame = tk.Frame(self.frame)
-        self.ec_a_label = tk.Label(self.ec_frame, text="a =")
-        self.ec_a = tk.Entry(self.ec_frame, width=5)
-        self.ec_b_label = tk.Label(self.ec_frame, text="b =")
-        self.ec_b = tk.Entry(self.ec_frame, width=5)
-        self.ec_button_ready = tk.Button(self.frame, text="Done", command=lambda: self.ec_ready())
+        self.ec_a_label = tk.Label(self.ec_frame)
+        self.ec_a_entry = tk.Entry(self.ec_frame)
+        self.ec_b_label = tk.Label(self.ec_frame)
+        self.ec_b_entry = tk.Entry(self.ec_frame)
+        self.ec_ready_button = tk.Button(self.frame)
         self.ec_eq_text = tk.StringVar()
-        self.ec_eq = tk.Label(self.frame, textvariable=self.ec_eq_text)
-        self.ec_button_edit = tk.Button(self.frame, text="Edit", command=lambda: self.ec_clear())
+        self.ec_eq = tk.Label(self.frame)
+        self.ec_button_edit = tk.Button(self.frame)
 
         self.ec_set()
 
@@ -39,28 +38,17 @@ class Ecdh:
         blank2.grid(row=6, column=0)
 
         # ///////////// Begin Generator Point /////////////
-        self.g_point = tk.Label(self.frame, text="Step 2: choose generator point to be used by both Bob and Alice", state='disabled')
-        self.g_point.grid(row=7, column=0)
-
+        self.g_title = tk.Label(self.frame)
         self.g_frame = tk.Frame(self.frame)
-
-        g_x_label = tk.Label(self.g_frame, text="x =")
-        g_x_label.pack(side=tk.LEFT)
-        self.g_x = tk.Entry(self.g_frame, width=5, state='disabled')
-        self.g_x.pack(side=tk.LEFT)
-
-        g_y_label = tk.Label(self.g_frame, text="y =")
-        g_y_label.pack(side=tk.LEFT)
-        self.g_y = tk.Entry(self.g_frame, width=5, state='disabled')
-        self.g_y.pack(side=tk.LEFT)
-
-        self.g_frame.grid(row=8, column=0)
-        self.g_button = tk.Button(self.frame, text="Done", state='disabled', command=lambda: self.g_ready())
-        self.g_button.grid(row=9, column=0)
-
+        self.g_x_label = tk.Label(self.g_frame)
+        self.g_x_entry = tk.Entry(self.g_frame)
+        self.g_y_label = tk.Label(self.g_frame)
+        self.g_y_entry = tk.Entry(self.g_frame)
+        self.g_ready_button = tk.Button(self.frame)
         self.g_text = tk.StringVar()
-        self.g_label = tk.Label(self.frame, textvariable=self.g_text)
-        self.g_label.grid(row=9, column=1)
+        self.g_label = tk.Label(self.frame)
+
+        self.g_set()
 
         # ///////////// End Generator Point /////////////
 
@@ -74,20 +62,20 @@ class Ecdh:
                              state='disabled')
         self.step3_label.grid(row=12, column=0)
 
-        self.bob_title = tk.Label(self.frame, text="Bob:", state='disabled')
+        self.bob_title = tk.Label(self.frame, text="Bob:", state="disabled")
         self.bob_title.grid(row=13, column=0)
 
-        self.bob_label_priv = tk.Label(self.frame, text="Private Key:", state='disabled')
+        self.bob_label_priv = tk.Label(self.frame, text="Private Key:", state="disabled")
         self.bob_label_priv.grid(row=14, column=0)
 
-        self.bob_entry_priv = tk.Entry(self.frame, state='disabled')
+        self.bob_entry_priv = tk.Entry(self.frame, state="disabled")
         self.bob_entry_priv.grid(row=14, column=1)
 
-        self.bob_label_pub = tk.Label(self.frame, text="Public Key:", state='disabled')
+        self.bob_label_pub = tk.Label(self.frame, text="Public Key:", state="disabled")
         self.bob_label_pub.grid(row=15, column=0)
 
         self.bob_pub_text = tk.StringVar()
-        self.bob_label = tk.Label(self.frame, textvariable=self.bob_pub_text, state='disabled')
+        self.bob_label = tk.Label(self.frame, textvariable=self.bob_pub_text, state="disabled")
         self.bob_label.grid(row=15, column=1)
 
         # ///////////// End Bob /////////////
@@ -98,27 +86,27 @@ class Ecdh:
         # ///////////// Begin Alice /////////////
         self.alice = ec.User()
 
-        self.alice_title = tk.Label(self.frame, text="Alice:", state='disabled')
+        self.alice_title = tk.Label(self.frame, text="Alice:", state="disabled")
         self.alice_title.grid(row=17, column=0)
 
-        self.alice_label_priv = tk.Label(self.frame, text="Private Key:", state='disabled')
+        self.alice_label_priv = tk.Label(self.frame, text="Private Key:", state="disabled")
         self.alice_label_priv.grid(row=18, column=0)
 
-        self.alice_entry_priv = tk.Entry(self.frame, state='disabled')
+        self.alice_entry_priv = tk.Entry(self.frame, state="disabled")
         self.alice_entry_priv.grid(row=18, column=1)
 
-        self.alice_label_pub = tk.Label(self.frame, text="Public Key:", state='disabled')
+        self.alice_label_pub = tk.Label(self.frame, text="Public Key:", state="disabled")
         self.alice_label_pub.grid(row=19, column=0)
 
         self.alice_pub_text = tk.StringVar()
-        self.alice_pub_label = tk.Label(self.frame, textvariable=self.alice_pub_text, state='disabled')
+        self.alice_pub_label = tk.Label(self.frame, textvariable=self.alice_pub_text, state="disabled")
         self.alice_pub_label.grid(row=19, column=1)
 
-        self.bob_alice_button = tk.Button(self.frame, text="Done", command=lambda: self.bob_alice_ready(), state='disabled')
+        self.bob_alice_button = tk.Button(self.frame, text="Done", command=lambda: self.bob_alice_ready(), state="disabled")
         self.bob_alice_button.grid(row=20, column=0)
 
         self.bob_alice_text = tk.StringVar()
-        self.bob_alice_label = tk.Label(self.frame, textvariable=self.bob_alice_text, state='disabled')
+        self.bob_alice_label = tk.Label(self.frame, textvariable=self.bob_alice_text, state="disabled")
         self.bob_alice_label.grid(row=20, column=3)
 
         # ///////////// End Alice /////////////
@@ -128,19 +116,19 @@ class Ecdh:
 
         # ///////////// Begin Shared Calculations /////////////
         self.step4_label = tk.Label(self.frame, text="Step 4: generate same shared key by Bob and Alice separately",
-                                    state='disabled')
+                                    state="disabled")
         self.step4_label.grid(row=22, column=0)
 
-        self.shared_bob = tk.Label(self.frame, text="Shared key calculated by Bob: ", state='disabled')
+        self.shared_bob = tk.Label(self.frame, text="Shared key calculated by Bob: ", state="disabled")
         self.shared_bob.grid(row=23, column=0)
         self.shared_bob_text = tk.StringVar()
         self.shared_label_bob = tk.Label(self.frame, textvariable=self.shared_bob_text)
         self.shared_label_bob.grid(row=23, column=1)
 
-        self.shared_alice = tk.Label(self.frame, text="Shared key calculated by Alice: ", state='disabled')
+        self.shared_alice = tk.Label(self.frame, text="Shared key calculated by Alice: ", state="disabled")
         self.shared_alice.grid(row=24, column=0)
         self.shared_alice_text = tk.StringVar()
-        self.shared_label_alice = tk.Label(self.frame, textvariable=self.shared_alice_text, state='disabled')
+        self.shared_label_alice = tk.Label(self.frame, textvariable=self.shared_alice_text, state="disabled")
         self.shared_label_alice.grid(row=24, column=1)
 
 
@@ -149,22 +137,37 @@ class Ecdh:
     def ec_set(self):
         self.ec_title.config(text='Step 1: choose the elliptic curve to be used by both Bob and Alice')
         self.ec_title.grid(row=2, column=0, sticky="W")
+
         self.ec_gen_eq.set("y\u00B2 = x\u00B3 + ax + b")
+        self.ec_gen_eq_label.config(textvariable=self.ec_gen_eq)
         self.ec_gen_eq_label.grid(row=3, column=0)
+
+        self.ec_a_label.config(text="a =")
         self.ec_a_label.pack(side=tk.LEFT)
-        self.ec_a.pack(side=tk.LEFT)
+        self.ec_a_entry.config(width=5)
+        self.ec_a_entry.pack(side=tk.LEFT)
+
+        self.ec_b_label.config(text="b =")
         self.ec_b_label.pack(side=tk.LEFT)
-        self.ec_b.pack(side=tk.LEFT)
+        self.ec_b_entry.pack(side=tk.LEFT)
+        self.ec_b_entry.config(width=5)
+
         self.ec_frame.grid(row=4, column=0)
-        self.ec_button_ready.grid(row=5, column=0)
+
+        self.ec_ready_button.config(text="Done", command=lambda: self.ec_ready())
+        self.ec_ready_button.grid(row=5, column=0)
+
         self.ec_eq.grid(row=5, column=1)
-        self.ec_button_edit.grid(row=5, column=2)
+
+        self.ec_eq.config(textvariable=self.ec_eq_text)
+
+        self.ec_button_edit.config(text="Edit", command=lambda: self.ec_clear())
         self.ec_button_edit.grid_forget()
 
     def ec_ready(self):
         try:
-            a_str = self.ec_a.get()
-            b_str = self.ec_b.get()
+            a_str = self.ec_a_entry.get()
+            b_str = self.ec_b_entry.get()
 
             if a_str == '' or b_str == '':
                 text = "a or b are empty"
@@ -190,27 +193,57 @@ class Ecdh:
             raise ValueError("invalid input")
 
         self.ec_eq_text.set(self.elliptic_curve.print())
-        self.g_point.config(state='normal')
-        self.g_x.config(state='normal')
-        self.g_y.config(state='normal')
-        self.g_button.config(state='normal')
-        self.ec_button_edit.grid(row=5, column=2)
+        self.ec_a_entry.config(state="disabled")
+        self.ec_b_entry.config(state="disabled")
+        self.ec_ready_button.grid_forget()
+        self.ec_button_edit.grid(row=5, column=0)
+
+        self.g_title.config(state='normal')
+        self.g_x_label.config(state='normal')
+        self.g_y_label.config(state='normal')
+        self.g_x_entry.config(state='normal')
+        self.g_y_entry.config(state='normal')
+        self.g_ready_button.config(state='normal')
+
 
     def ec_clear(self):
-        self.ec_a.delete(0, 'end')
-        self.ec_b.delete(0, 'end')
+        self.ec_a_entry.delete(0, 'end')
+        self.ec_a_entry.config(state="normal")
+        self.ec_b_entry.delete(0, 'end')
+        self.ec_b_entry.config(state="normal")
         self.ec_title.grid(row=2, column=0, sticky="W")
         self.ec_eq_text.set("")
-        self.ec_button_ready.grid(row=5, column=0)
+        self.ec_ready_button.grid(row=5, column=0)
         self.ec_eq.grid(row=5, column=1)
         self.ec_button_edit.grid_forget()
 
-    def set_g(self):
-        pass
+        self.g_clear()
+
+    def g_set(self):
+        self.g_title.config(text="Step 2: choose generator point to be used by both Bob and Alice", state="disabled")
+        self.g_title.grid(row=7, column=0)
+
+        self.g_x_label.config(text="x =")
+        self.g_x_label.pack(side="left")
+        self.g_x_entry.config(width=5, state="disabled")
+        self.g_x_entry.pack(side="left")
+
+        self.g_y_label.config(text="y =")
+        self.g_y_label.pack(side="left")
+        self.g_y_entry.config(width=5, state="disabled")
+        self.g_y_entry.pack(side="left")
+
+        self.g_frame.grid(row=8, column=0)
+
+        self.g_ready_button.config(text="Done", state="disabled", command=lambda: self.g_ready())
+        self.g_ready_button.grid(row=9, column=0)
+
+        self.g_label.config(textvariable=self.g_text)
+        self.g_label.grid(row=9, column=1)
 
     def g_ready(self):
-        x_str = self.g_x.get()
-        y_str = self.g_y.get()
+        x_str = self.g_x_entry.get()
+        y_str = self.g_y_entry.get()
         try:
 
             if x_str == '' or y_str == '':
@@ -253,6 +286,19 @@ class Ecdh:
         self.alice_label_pub.config(state="normal")
 
         self.bob_alice_button.config(state="normal")
+
+    def g_clear(self):
+        self.g_title.config(state="disabled")
+        self.g_x_label.config(state="disabled")
+        self.g_x_entry.delete(0, "end")
+        self.g_x_entry.config(state="disabled")
+        self.g_y_label.config(state="disabled")
+        self.g_y_entry.delete(0, "end")
+        self.g_y_entry.config(state="disabled")
+        self.g_ready_button.config(state="disabled")
+        self.g_text.set("")
+        self.g_label.grid_forget()
+
 
     def bob_alice_ready(self):
         bob_priv_str = self.bob_entry_priv.get()
