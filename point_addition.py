fe01657396b9fba193a9da0fe5d113f092d5d1a2
omar <OmarGH>
2021-12-22 17:24:36 -0300

    un ejemplo de efecto hovering con puntos funcionando

--- point_addition.py
+++ point_addition.py
@@ -6,6 +6,8 @@ from PIL import Image, ImageTk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 NavigationToolbar2Tk)
+import matplotlib.pyplot as plt
+import numpy as np; np.random.seed(1)
 
 
 class PointAddition:
@@ -32,6 +34,50 @@ class PointAddition:
     # plotting the graph in
     # tkinter window
     def plot(self):
+        x = np.random.rand(15)
+        y = np.random.rand(15)
+        names = np.array(list("ABCDEFGHIJKLMNO"))
+        c = np.random.randint(1, 5, size=15)
+
+        norm = plt.Normalize(1, 4)
+        cmap = plt.cm.RdYlGn
+
+        fig, ax = plt.subplots()
+        sc = plt.scatter(x, y, c=c, s=100, cmap=cmap, norm=norm)
+
+        annot = ax.annotate("", xy=(0, 0), xytext=(20, 20), textcoords="offset points",
+                            bbox=dict(boxstyle="round", fc="w"),
+                            arrowprops=dict(arrowstyle="->"))
+        annot.set_visible(False)
+
+        def update_annot(ind):
+
+            pos = sc.get_offsets()[ind["ind"][0]]
+            annot.xy = pos
+            text = "{}, {}".format(" ".join(list(map(str, ind["ind"]))),
+                                   " ".join([names[n] for n in ind["ind"]]))
+            annot.set_text(text)
+            annot.get_bbox_patch().set_facecolor(cmap(norm(c[ind["ind"][0]])))
+            annot.get_bbox_patch().set_alpha(0.4)
+
+        def hover(event):
+            vis = annot.get_visible()
+            if event.inaxes == ax:
+                cont, ind = sc.contains(event)
+                if cont:
+                    update_annot(ind)
+                    annot.set_visible(True)
+                    fig.canvas.draw_idle()
+                else:
+                    if vis:
+                        annot.set_visible(False)
+                        fig.canvas.draw_idle()
+
+        fig.canvas.mpl_connect("motion_notify_event", hover)
+
+        plt.show()
+
+        '''
         # the figure that will contain the plot
         fig = Figure(figsize=(5, 5),
                      dpi=100)
@@ -61,6 +107,7 @@ class PointAddition:
 
         # placing the toolbar on the Tkinter window
         canvas.get_tk_widget().pack()
+        '''
 
     def start_page(self):
         self.frame.grid(column=0, row=0, sticky="NWES")
