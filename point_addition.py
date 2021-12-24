b4de7d9852b0e02fd94511f1f10deaf6863852c4
omar <OmarGH>
2021-12-24 11:05:44 -0300

    primers pruebas de gráfico interactivo

--- point_addition.py
+++ point_addition.py
@@ -8,7 +8,87 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 NavigationToolbar2Tk)
 import matplotlib.pyplot as plt
 import numpy as np; np.random.seed(1)
+from numpy.random import rand
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+from matplotlib.text import Text
+import math
 
+
+class AnnoteFinder(object):
+    """callback for matplotlib to display an annotation when points are
+    clicked on.  The point which is closest to the click and within
+    xtol and ytol is identified.
+
+    Register this function like this:
+
+    scatter(xdata, ydata)
+    af = AnnoteFinder(xdata, ydata, annotes)
+    connect('button_press_event', af)
+    """
+
+    def __init__(self, xdata, ydata, annotes, ax=None, xtol=None, ytol=None):
+        self.data = list(zip(xdata, ydata, annotes))
+        if xtol is None:
+            xtol = ((max(xdata) - min(xdata)) / float(len(xdata))) / 2
+        if ytol is None:
+            ytol = ((max(ydata) - min(ydata)) / float(len(ydata))) / 2
+        self.xtol = xtol
+        self.ytol = ytol
+        if ax is None:
+            self.ax = plt.gca()
+        else:
+            self.ax = ax
+        self.drawnAnnotations = {}
+        self.links = []
+
+    def distance(self, x1, x2, y1, y2):
+        """
+        return the distance between two points
+        """
+        return (math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
+
+    def __call__(self, event):
+
+        if event.inaxes:
+
+            clickX = event.xdata
+            clickY = event.ydata
+            if (self.ax is None) or (self.ax is event.inaxes):
+                annotes = []
+                # print(event.xdata, event.ydata)
+                for x, y, a in self.data:
+                    # print(x, y, a)
+                    if ((clickX - self.xtol < x < clickX + self.xtol) and
+                            (clickY - self.ytol < y < clickY + self.ytol)):
+                        annotes.append(
+                            (self.distance(x, clickX, y, clickY), x, y, a))
+                if annotes:
+                    annotes.sort()
+                    distance, x, y, annote = annotes[0]
+                    self.drawAnnote(event.inaxes, x, y, annote)
+                    for l in self.links:
+                        l.drawSpecificAnnote(annote)
+
+    def drawAnnote(self, ax, x, y, annote):
+        """
+        Draw the annotation on the plot
+        """
+        if (x, y) in self.drawnAnnotations:
+            markers = self.drawnAnnotations[(x, y)]
+            for m in markers:
+                m.set_visible(not m.get_visible())
+            self.ax.figure.canvas.draw_idle()
+        else:
+            t = ax.text(x, y, " - %s" % (annote), )
+            m = ax.scatter([x], [y], marker='d', c='r', zorder=100)
+            self.drawnAnnotations[(x, y)] = (t, m)
+            self.ax.figure.canvas.draw_idle()
+
+    def drawSpecificAnnote(self, annote):
+        annotesToDraw = [(x, y, a) for x, y, a in self.data if a == annote]
+        for x, y, a in annotesToDraw:
+            self.drawAnnote(self.ax, x, y, a)
 
 class PointAddition:
     def __init__(self, master=None, app=None):
@@ -34,47 +114,14 @@ class PointAddition:
     # plotting the graph in
     # tkinter window
     def plot(self):
-        x = np.random.rand(15)
-        y = np.random.rand(15)
-        names = np.array(list("ABCDEFGHIJKLMNO"))
-        c = np.random.randint(1, 5, size=15)
-
-        norm = plt.Normalize(1, 4)
-        cmap = plt.cm.RdYlGn
+        x = (1, 2, 3, 4)
+        y = (1, 2, 3, 4)
+        annotes = ['a', 'b', 'c', 'd']
 
         fig, ax = plt.subplots()
-        sc = plt.scatter(x, y, c=c, s=100, cmap=cmap, norm=norm)
-
-        annot = ax.annotate("", xy=(0, 0), xytext=(20, 20), textcoords="offset points",
-                            bbox=dict(boxstyle="round", fc="w"),
-                            arrowprops=dict(arrowstyle="->"))
-        annot.set_visible(False)
-
-        def update_annot(ind):
-
-            pos = sc.get_offsets()[ind["ind"][0]]
-            annot.xy = pos
-            text = "{}, {}".format(" ".join(list(map(str, ind["ind"]))),
-                                   " ".join([names[n] for n in ind["ind"]]))
-            annot.set_text(text)
-            annot.get_bbox_patch().set_facecolor(cmap(norm(c[ind["ind"][0]])))
-            annot.get_bbox_patch().set_alpha(0.4)
-
-        def hover(event):
-            vis = annot.get_visible()
-            if event.inaxes == ax:
-                cont, ind = sc.contains(event)
-                if cont:
-                    update_annot(ind)
-                    annot.set_visible(True)
-                    fig.canvas.draw_idle()
-                else:
-                    if vis:
-                        annot.set_visible(False)
-                        fig.canvas.draw_idle()
-
-        fig.canvas.mpl_connect("motion_notify_event", hover)
-
+        ax.scatter(x, y)
+        af = AnnoteFinder(x, y, annotes, ax=ax)
+        fig.canvas.mpl_connect('button_press_event', af)
         plt.show()
 
         '''
